a05078f0bbddcac9625b09ed948f9ae15d0fd7f9
Felipe <A01662377@tec.mx>
2025-10-30 12:33:45 -0600

    cambio tanto del color de la serpiente como el de la comida de manera random

--- vibora.py
+++ vibora.py
@@ -1,10 +1,14 @@
 from turtle import *
-from random import randrange
+from random import randrange, choice
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
+colors = ['blue', 'red', 'green', 'yellow', 'purple', 'orange']
+snake_color = choice(colors)
+food_color = choice([ c for c in colors if c != snake_color])
 
 def change(x, y):
     "Change snake direction."
@@ -37,9 +41,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, snake_color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
     ontimer(move, 100)
 
